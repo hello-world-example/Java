d58d846d87f9a6cf87471ba37ef8a33a0f09baad
杨凯彬 <kaibin.yang@ttpai.com>
2018-04-10 19:45:13 +0800

    Serializable

--- src/main/java/xyz/kail/javase/demo/proxy/subject/RealSubject.java
+++ src/main/java/xyz/kail/javase/demo/proxy/subject/RealSubject.java
@@ -1,9 +1,11 @@
 package xyz.kail.javase.demo.proxy.subject;
 
+import java.io.Serializable;
+
 /**
  * 真正执行任务的类，实现了代理接口。
  */
-public class RealSubject implements Subject {
+public class RealSubject implements Subject, Serializable {
 
     /**
      * 执行给定名字的任务。这里打印出任务名，并休眠500ms模拟任务执行了很长时间
