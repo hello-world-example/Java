eed19f3990b4c2b4d10de633861907d663e801d6
杨凯彬 <ykb553@163.com>
2019-05-01 18:38:46 +0800

    pom

---
+++ javase-tools/javase-tool-httpclient/src/main/java/xyz/kail/demo/java/se/tool/httpclient/Main.java
@@ -0,0 +1,11 @@
+package xyz.kail.demo.java.se.tool.httpclient;
+
+import org.apache.http.impl.client.HttpClients;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        HttpClients.custom()
+    }
+
+}
